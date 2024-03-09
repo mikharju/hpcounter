ae98136aa8ac828bf2497cd5506ce7ca4273ceef
mikharju <mikharju@gmail.com>
2024-03-09 10:55:46 +0200

    initializr

---
+++ src/main/java/fi/harjum/redux/hpcounter/HpcounterApplication.java
@@ -0,0 +1,13 @@
+package fi.harjum.redux.hpcounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HpcounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HpcounterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fi/harjum/redux/hpcounter/HpcounterApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.harjum.redux.hpcounter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HpcounterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
